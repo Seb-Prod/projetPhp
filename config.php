94629890ed4aa1d87bdb0258d9769d8a778e7048
Seb-Prod <sebastien.drillaud@gmail.com>
2025-02-07 10:30:09 +0100

    maj

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ if (!defined('SECURE_ACCESS')) {
 // Configuration de la base de données
 define('DB_HOST', 'localhost'); // Adresse du serveur
 define('DB_USER', 'root');      // Nom d'utilisateur MySQL
-define('DB_PASS', 'fff'); // Mot de passe
+define('DB_PASS', ''); // Mot de passe
 define('DB_NAME', 'base_voiture');   // Nom de la base de données
 
 // Fonction pour se connecter à la base de données
--- logIn.php
+++ logIn.php
@@ -18,8 +18,8 @@ if ($pdo) {
 <div>
     <form>
         <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <label for="">Email address</label>
+            <input type="text" class="form-control" id="" placeholder="Enter email">
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
